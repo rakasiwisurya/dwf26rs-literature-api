3d071e0b5b70d24a5bbb23d3b85654b8f5f9a952
Rakasiwi Surya <rakasiwi.surya@gmail.com>
2021-11-24 06:41:10 +0700

    done prepare model user

---
+++ seeders/20211123233144-user.js
@@ -0,0 +1,49 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+
+    await queryInterface.bulkInsert("users", [
+      {
+        email: "admin@gmail.com",
+        password:
+          "$2a$10$/DAeUmDBtraAci5tg5Y.AOifekzKfkowhqn7WxBITLWUkP0OxSBxK", //12345
+        fullname: "Admin",
+        gender: "male",
+        phone: "085717573808",
+        address: "Jl. Kenangan No. 1",
+        avatar: null,
+      },
+      {
+        email: "user1@gmail.com",
+        password:
+          "$2a$10$/DAeUmDBtraAci5tg5Y.AOifekzKfkowhqn7WxBITLWUkP0OxSBxK", //12345
+        fullname: "User1",
+        gender: "female",
+        phone: "085717573808",
+        address: "Jl. Melati No. 1",
+        avatar: null,
+      },
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+
+    await queryInterface.bulkDelete("users", null, {});
+  },
+};
