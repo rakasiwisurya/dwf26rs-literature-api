738f4a82ed0c9400f491d91acdd215a81ad02b5f
Rakasiwi Surya <rakasiwi.surya@gmail.com>
2021-11-24 09:49:29 +0700

    add auth middleware at get literatures

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,10 @@ const router = express.Router();
 const { login, register } = require("../controller/auth");
 const { getLiteratures } = require("../controller/literature");
 
+const { auth } = require("../../middlewares/auth");
+
 // router literature
-router.get("/literatures", getLiteratures);
+router.get("/literatures", auth, getLiteratures);
 
 // router auth
 router.post("/login", login);
